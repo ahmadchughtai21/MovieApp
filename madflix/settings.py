6320d97aabee23e0959b9a3f3536a7c052e4ff2c
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-08-29 01:26:24 +0500

    chore: Update static files configuration in settings.py

--- madflix/settings.py
+++ madflix/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = 'static',
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
