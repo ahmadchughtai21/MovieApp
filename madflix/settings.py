872aba57ae9ff5dde5288c95bdc2a128b99200e7
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-08-29 01:31:08 +0500

    chore: Update STATICFILES_DIRS in settings.py to include BASE_DIR for static files

--- madflix/settings.py
+++ madflix/settings.py
@@ -91,7 +91,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = 'static',
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
