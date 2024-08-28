66e4e0084ea91bfcdfb22a68c779c60ad9bb9675
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-08-29 00:58:47 +0500

    chore: Comment out django_browser_reload in settings.py and urls.py

--- madflix/urls.py
+++ madflix/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("", include("madflixapp.urls")),
     
     
-    path("__reload__/", include("django_browser_reload.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
 ]
